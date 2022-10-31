4f38841f83b9b0976ebc3dcc04127d01bb6255b5
fejjybanez <fejieybanez@gmail.com>
2022-10-31 19:10:45 +0800

    Added Gallery Page and Setup Blade directives

---
+++ resources/views/gallery.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+ 
+ 
+@section('content')
+
+  <!-- Masthead-->
+  <header class="masthead bg-primary text-white text-center">
+            <div class="container d-flex align-items-center flex-column">
+            <h1>Hello World</h1>
+            </div>
+        </header>
+        <!-- Portfolio Section-->
+        
+
+
+
+
+@endsection
